926dbadd2134e8e0a71730fec2f6b7d8b141cb4b
ksg9482 <ksg930523@gmail.com>
2022-04-04 23:48:03 +0900

    client setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { LoggedOutRouter } from './routers/logged-out-router';
+
+
+function App() {
+  return (
+    <LoggedOutRouter />
+  );
+}
+
+export default App;
